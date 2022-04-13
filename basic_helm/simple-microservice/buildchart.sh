55d6b7cefa0ec7d12bd5613e3905cad64e699594
varkrish <--get-all>
2022-04-13 20:45:55 +1000

    removing build chart

--- basic_helm/simple-microservice/buildchart.sh
+++
@@ -1,6 +0,0 @@
-rm deployment/charts/*
-cd /Users/varunkrishnamurthy/projects/dbs/dbs-cicd-common-helm
-helm package chart/  
-cp common_ci_template-1.0.0.tgz /Users/varunkrishnamurthy/projects/labs-enablement-spring-ocp/basic_helm/simple-microservice/deployment/charts/
-cd - 
-helm template deployment --debug

340d8e8b2fc8622acd37dd2b0ce2c1bc2fabfe44
varkrish <--get-all>
2022-05-17 13:40:50 +1000

    Feat + observavility

--- basic/simple-microservice/src/main/java/com/redhat/openinnovationlabs/microservice/RestService.java
+++ basic/simple-microservice/src/main/java/com/redhat/openinnovationlabs/microservice/RestService.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class RestService {
 
@@ -14,12 +20,19 @@ public class RestService {
 	private final AtomicLong counter = new AtomicLong();
 	@Value("${app.name:myapp}")
 	private String applicationName;
+    
+	Logger logger = LoggerFactory.getLogger(RestService.class);
+
 	@GetMapping("/rest")
+	@Timed(value="greeting.timed")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.info("/rest called");
 		return " " + counter.incrementAndGet() + ". " +   String.format(template, name);
 	}
 	@GetMapping("/appname")
+	@Timed(value="getName.timed")
 	public String getName() {
+		logger.info(applicationName);
 		return applicationName;
 	}
 	
